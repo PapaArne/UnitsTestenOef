9acdc3d591d03de0a82a255fa5204999c34dfb89
PapaArne <43129636+PapaArne@users.noreply.github.com>
2020-05-10 17:49:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTests{    
+[TestClass]
+    public class BankAccountTests    
+    {        [TestMethod] 
+        publicvoidTestMethod1() {
+            try {
+                double getal1;
+                double getal2;
+                double uitkomst;
+                uitkomst = getal1 / getal2;
+            }
+            catch(devidedByzeroExeption)
+            {
+                global::System.Console.WriteLine("Cannot devide by zero");
+            }
+        }    }
+
+}
